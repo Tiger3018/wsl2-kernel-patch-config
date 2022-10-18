a56d841689abf0173ae08b874f9c1750074c2d5f
Tiger3018 <tiger3018of02@gmail.com>
2022-10-19 02:39:25 +0800

    debug: -jn and upload files

--- usb_cam.sh
+++ usb_cam.sh
@@ -7,5 +7,5 @@ patch kernel-5.10/Microsoft/config-wsl usb_cam-5.10.patch
 mkdir -p build
 
 pushd kernel-5.10
-make KCONFIG_CONFIG=Microsoft/config-wsl O=../build
+make KCONFIG_CONFIG=Microsoft/config-wsl O=../build -j$(getconf _NPROCESSORS_ONLN)
 popd
