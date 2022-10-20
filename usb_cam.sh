c6e64fd8216caae1ef7b13ed569b79e5b4dbeeda
Tiger3018 <tiger3018of02@gmail.com>
2022-10-20 12:27:14 +0800

    test: fakeroot

--- usb_cam.sh
+++ usb_cam.sh
@@ -4,8 +4,14 @@ sudo apt-get update && sudo apt-get install build-essential flex bison dwarves l
 
 patch kernel-5.10/Microsoft/config-wsl usb_cam-5.10.patch
 
-mkdir -p build
+mkdir -p build fakeroot
 
 pushd kernel-5.10
-make KCONFIG_CONFIG=Microsoft/config-wsl O=../build -j$(getconf _NPROCESSORS_ONLN)
+    INSTALL_PATH=../fakeroot \
+        make all KCONFIG_CONFIG=Microsoft/config-wsl O=../build -j$(getconf _NPROCESSORS_ONLN)
+popd
+shopt -s globstar
+pushd build
+    ls **/.tmp*
+    rm -f **/.tmp*
 popd
